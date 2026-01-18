7655dabed977d50c253ba4c57930b28fff046ac6
f4anks <frankhsistemas@gmail.com>
2026-01-18 17:13:51 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,26 @@
-const CACHE_NAME = 'control-voley-v1';
+const CACHE_NAME = 'voley-v1';
 const ASSETS = [
-  './',
-  './index.html',
-  './index_menu.html',
-  './index_atletas.html',
-  './index_datos.html',
-  './index_ficha.html',
-  './index_cumple.html',
-  './index_estad.html',
-  './img/banner.png'
+  '/',
+  '/index.html',
+  '/index_menu.html',
+  '/index_atletas.html',
+  '/index_datos.html',
+  '/index_ficha.html',
+  '/index_cumple.html',
+  '/index_estad.html',
+  '/img/banner.png'
 ];
 
-// Instalación: Guardar archivos en caché
+// Instalación
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-// Estrategia de carga: Primero caché, luego red
+// Estrategia: Red primero, si falla, usa caché
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
